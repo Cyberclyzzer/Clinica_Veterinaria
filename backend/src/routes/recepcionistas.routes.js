81a0cc458a303c126382f767984726e6a3456fad
FM <fmejia22@ujap.edu.ve>
2025-05-06 23:02:39 -0400

    Algunos errores corregidos

--- backend/src/routes/recepcionistas.routes.js
+++ backend/src/routes/recepcionistas.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const recepcionistasController = require('../controllers/recepcionistas.controller');
-const postValidator = require('../validators/recepcionistas.validator');
+const { postValidator } = require('../validators/recepcionistas.validator');
 
-router.get('/', postValidator.recepcionistasController.listarRecepcionistas);
+router.get('/', recepcionistasController.listarRecepcionistas);
 
 router.get('/:id', recepcionistasController.obtenerRecepcionista);
 
-router.post('/', recepcionistasController.crearRecepcionista);
+router.post('/', postValidator, recepcionistasController.crearRecepcionista);
 
 module.exports = router;
